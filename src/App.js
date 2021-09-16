0b925be7885cb59f926049287b3d65908489b20f
Çağla Özdemir <oezdemircaglayagmur@gmail.com>
2021-09-16 08:25:38 +0300

    background vol-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from './components/header/header';
 import './components/header/header.css'
+import background from './background.jpg';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='HomePage'>
+        <div className='background'>
+          <div style={{backgroundImage: `url(${background})`, width: '100%', height:'500px'}} alt="background">
+            <h1>hey</h1>
+          </div> 
+        </div>
+      </div>
     </div>
   );
 }
